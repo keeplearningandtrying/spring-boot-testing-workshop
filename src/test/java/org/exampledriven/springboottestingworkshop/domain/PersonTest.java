5ccc5ec05d55c48919fba2b6e99f9992cefde628
Peter Szanto <git@szantocsalad.hu>
2017-07-04 13:19:59 +0200

    Adding @JsonTest

---
+++ src/test/java/org/exampledriven/springboottestingworkshop/domain/PersonTest.java
@@ -0,0 +1,11 @@
+package org.exampledriven.springboottestingworkshop.domain;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Peter Szanto on 7/4/2017.
+ */
+public class PersonTest {
+    //TODO implement
+
+}
