ffe07647067ebfa27e1e9441c8d9585fa985c648
Peter Szanto <git@szantocsalad.hu>
2017-07-04 13:51:13 +0200

    Adding OutputCapture test

---
+++ src/test/java/org/exampledriven/springboottestingworkshop/service/BlackBoxServiceTest.java
@@ -0,0 +1,21 @@
+package org.exampledriven.springboottestingworkshop.service;
+
+import org.exampledriven.springboottestingworkshop.client.PostClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Peter Szanto on 7/4/2017.
+ */
+public class BlackBoxServiceTest {
+
+    @Test
+    public void doUntestableThings() throws Exception {
+        //TODO implement
+    }
+
+}
