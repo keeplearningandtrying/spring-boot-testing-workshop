b667fb0acb20b7002604c5c0d26e5d4553755da9
Peter Szanto <git@szantocsalad.hu>
2017-07-04 13:21:40 +0200

    Adding RestController and tests

---
+++ src/test/java/org/exampledriven/springboottestingworkshop/controller/PostControllerTest.java
@@ -0,0 +1,16 @@
+package org.exampledriven.springboottestingworkshop.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Peter Szanto on 7/4/2017.
+ */
+public class PostControllerTest {
+    @Test
+    public void readPost() throws Exception {
+        //TODO implement
+    }
+
+}
